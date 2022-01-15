39cf911394ca24b7be444ef04f77c7271c6375c2
master-code-java <delllg1122@yahoo.com>
2022-01-15 17:19:11 +0000

    cleaning some sql

--- src/main/java/com/caceis/capacityplan/repository/DashboardRepository.java
+++ src/main/java/com/caceis/capacityplan/repository/DashboardRepository.java
@@ -11,18 +11,18 @@ import java.util.Map;
 
 public interface DashboardRepository extends CrudRepository<SprintAndTask, Long> {
 
-    @Query(value = "select \n" +
-            "        prj.name \t\t\t\t\t\tas project_name,\n" +
-            "        sum(t.estimation)\t\t\t\tas amount_of_hours\n" +
-            "from \n" +
-            "\tcapacity.sprint_and_task st \n" +
-            "inner join task t\n" +
+    @Query(value = "SELECT \n" +
+            "        prj.name as project_name,\n" +
+            "        SUM(t.estimation) as amount_of_hours\n" +
+            "FROM \n" +
+            "capacity.sprint_and_task st \n" +
+            "INNER JOIN task t\n" +
             "on t.uuid=st.task_uuid\n" +
-            "inner join project prj\n" +
+            "INNER JOIN project prj\n" +
             "on t.project_uuid=prj.uuid  \n" +
-            "where \n" +
-            "\tst.sprint_uuid=\"d61c6060-951a-4f81-afc1-e1503bb7a811\"\n" +
-            "group by prj.name",nativeQuery = true)
+            "WHERE \n" +
+            "st.sprint_uuid=:sprintUuid\n" +
+            "GROUP BY prj.name",nativeQuery = true)
     List<Map<String, Long>> getCapacityPerProject(@Param("sprintUuid") String sprintUuid);
 
 }
